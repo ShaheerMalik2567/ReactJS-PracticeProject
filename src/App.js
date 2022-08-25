0835e759553b59d3ffe4c2c9c2b98ac166d0e3ac
sherjilmalik <sherjil.malik@ideofuzion.com>
2022-08-25 18:49:11 +0500

    Init

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+import AddUser from "./Users/AddUser";
+import UserList from "./Users/UserList";
+
+const App = () => {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={usersList} />
+    </div>
+  );
+};
+
+export default App;
